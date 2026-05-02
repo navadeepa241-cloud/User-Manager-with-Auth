e71d80b304afc8612e722e364b16af0ff13834c4
navadeepa241-cloud <navadeepa241@gnail.com>
2026-05-02 13:12:37 +0530

    fixed port issue

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 require('dotenv').config();
-const express = require('express'); 
-const cors = require('cors'); 
-const connectDB = require('./db');
-  
-const app = express(); 
-  
-app.use(cors()); 
-app.use(express.json()); 
-  
-connectDB(); 
-  
-app.use('/api/users', require('./routes/userRoutes')); 
-  
-app.listen(5000, () => console.log(`http://localhost:5000`))
+
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.listen(5000, () => console.log("Server running"));
